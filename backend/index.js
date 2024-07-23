937c5fedd12212f9b14e0de9570156a8a4fd7e38
shreyaskodial <shreyas@exalogic.co>
2024-07-23 22:59:51 +0530

    firt commit

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+﻿const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config({});
+
+
+const app = express();
+
+
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true
+};
+
+app.use(cors(corsOptions));
+
+const PORT =process.env.PORT|| 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+});
